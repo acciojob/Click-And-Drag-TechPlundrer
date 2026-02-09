54c6c9ba518a188636d356b065f45b00d479b480
Vishal Patil <vishalpatilvgp2199@gmail.com>
2026-02-09 11:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 
-  const container = document.querySelector('.items');
-  let draggedItem = null;
-
-  container.addEventListener('dragstart', (e) => {
-    if (!e.target.classList.contains('item')) return;
-
-    draggedItem = e.target;
-    e.dataTransfer.setData('text/plain', ''); 
-    e.dataTransfer.effectAllowed = 'move';
-  });
-
-  container.addEventListener('dragover', (e) => {
-    e.preventDefault(); 
-  });
-
-  container.addEventListener('drop', (e) => {
-    e.preventDefault();
-
-    const target = e.target.closest('.item');
-    if (!draggedItem || !target || draggedItem === target) return;
-
-    container.insertBefore(draggedItem, target.nextSibling);
-  });
-
-  container.addEventListener('dragend', () => {
-    draggedItem = null;
-  });
-
 
 
 
