d7d4958e919c646f8dfc6e9d8f154b78de46aec1
Vishal Patil <vishalpatilvgp2199@gmail.com>
2026-02-11 12:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
 
+let draggedItem = null;
+
+document.querySelectorAll('li').forEach(item => {
+
+  item.addEventListener('dragstart', function() {
+    draggedItem = this;
+  });
+
+  item.addEventListener('dragover', function(e) {
+    e.preventDefault();
+  });
+
+  item.addEventListener('drop', function() {
+    this.parentNode.insertBefore(draggedItem, this);
+  });
+
+});
 
 
